857ee2cce2b718f88d484309ee03a22eb33db255
robhogfeldt-fron15 <robhogfeldt@hotmail.com>
2016-06-15 19:35:31 +0200

    validation on reg

--- app_client/auth/register/register.controller.js
+++ app_client/auth/register/register.controller.js
@@ -7,7 +7,8 @@
   registerCtrl.$inject = ['$location', 'authentication'];
   function registerCtrl($location, authentication) {
     var vm = this;
-
+    vm.errorMessage = "Fyll i alla fält please :)";
+    vm.showError = false;
     vm.credentials = {
       name : "",
       email : "",
@@ -17,20 +18,60 @@
 
     vm.onSubmit = function () {
       console.log('Submitting registration');
-      authentication
-        .register(vm.credentials)
-        .error(function(err){
-          alert(err);
-        })
-        .then(function(res){
-          if (res.data.user.role === 'admin') {
-            $location.path('profile');
+      for (var key in vm.credentials) {
+      if (Object.prototype.hasOwnProperty.call(vm.credentials, key)) {
+          var val = vm.credentials[key];
+          if (val === '') {
+            vm.showError = true;
+            return;
           } else {
-            $location.path('user');
+            vm.showError = false;
+          authentication
+            .register(vm.credentials)
+            .error(function(err){
+              alert(err);
+            })
+            .then(function(res){
+              if (res.data.user.role === 'admin') {
+                $location.path('profile');
+              } else {
+                $location.path('user');
+              }
+            });
           }
-        });
-    };
 
+      }
+    }
+    };
   }
 
 })();
+
+
+// vm.onSubmit = function () {
+//   console.log('Submitting registration');
+//   for (var key in vm.credentials) {
+//   if (Object.prototype.hasOwnProperty.call(vm.credentials, key)) {
+//       var val = vm.credentials[key];
+//       if (val === '') {
+//         vm.showError = true;
+//         return;
+//       } else {
+//         vm.showError = false;
+//       authentication
+//         .register(vm.credentials)
+//         .error(function(err){
+//           alert(err);
+//         })
+//         .then(function(res){
+//           if (res.data.user.role === 'admin') {
+//             $location.path('profile');
+//           } else {
+//             $location.path('user');
+//           }
+//         });
+//       }
+//
+//   }
+// }
+// };
